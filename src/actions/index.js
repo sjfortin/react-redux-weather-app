1dd957e735dc4b2a98290eeb48ae0c4351f443a1
Sam Fortin <sam.j.fortin@gmail.com>
2017-12-17 21:17:54 -0600

    axios get request to openweathermap

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_Key = 'bcbb2e1504fc00d416b5c04b80f54e83';
+import axios from 'axios';
 
+const API_Key = 'bcbb2e1504fc00d416b5c04b80f54e83';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=appid=${API_Key}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
